96bb6d333c14dce7ca87cb996c5ea2a1b62e686d
prisma-ninja <cc.cellier@gmail.com>
2021-05-05 18:59:50 +0200

    add logo + integrated keyboard

--- app/src/main/java/com/example/onepopote/MainActivity.kt
+++ app/src/main/java/com/example/onepopote/MainActivity.kt
@@ -1,23 +1,35 @@
 package com.example.onepopote
 
 import android.graphics.Color
+import android.graphics.drawable.shapes.Shape
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color.Companion.Red
 import androidx.compose.ui.graphics.Color.Companion.Transparent
+import androidx.compose.ui.graphics.Color.Companion.White
+import androidx.compose.ui.graphics.Color.Companion.Yellow
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.em
-import androidx.compose.ui.unit.sp
 import com.example.onepopote.ui.*
+import org.intellij.lang.annotations.JdkConstants
 import java.lang.Integer.parseInt
 
 class MainActivity : ComponentActivity() {
@@ -26,64 +38,29 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             OnePopoteTheme {
-                Scaffold() {
-                    // A surface container using the 'background' color from the theme
-                    Surface(color = MaterialTheme.colors.background) {
-                        Column (
-                            verticalArrangement = Arrangement.Bottom,
-                            modifier = Modifier.fillMaxSize()
-                        ) {
+                Surface(color = MaterialTheme.colors.background) {
+                    Column (modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.Bottom) {
 
-                            var pasta by remember { mutableStateOf("") }
-                            var acqua = 0;
+                        var pasta by remember { mutableStateOf("") }
+                        var acqua = 0;
 
-                            try {
-                                acqua = (parseInt(pasta) * 540) / 227;
+                        try {
+                            acqua = (parseInt(pasta) * 540) / 227;
+                        }
+                        catch (e: NumberFormatException) {
+                            acqua = 0;
+                        }
+
+                        Row (modifier = Modifier.weight(2f), verticalAlignment = Alignment.CenterVertically) {
+                            BigDisplayer(acqua)
+                        }
+                        Row (modifier = Modifier.weight(4f)) {
+                            Surface(modifier = Modifier.fillMaxHeight(), color = dark200, shape = RoundedCornerShape(topStart = 20.dp, topEnd = 20.dp)) {
+                                Keyboard(pasta, onValueChange = { if (!pasta.isEmpty() || it != "0") { pasta += it } }, onReset = { pasta = it})
                             }
-                            catch (e: NumberFormatException) {
-                                acqua = 0;
-                            }
-
-                            Column() {
-                                Row (modifier = Modifier.weight(3f),
-                                    verticalAlignment = Alignment.CenterVertically) {
-                                    BigDisplayer(acqua)
-                                }
-                                Row (modifier = Modifier.weight(1f)
-                                ) {
-                                    Surface(modifier = Modifier.fillMaxSize(), color = dark200, shape = RoundedCornerShape(topStart = 20.dp, topEnd = 20.dp)) {
-                                        Column() {
-                                            Text("CUSTOMIZE", letterSpacing = 2.sp, modifier = Modifier.padding(15.dp), color = dark900)
-
-                                            Row(modifier = Modifier
-                                                .fillMaxSize()
-                                                .padding(bottom = 25.dp), horizontalArrangement = Arrangement.Center) {
-                                                TextField(
-                                                    value = pasta,
-                                                    onValueChange = { if (it.length < 5 ) pasta = it },
-                                                    label = { Text("Pasta (g)") },
-                                                    shape = RoundedCornerShape(30.dp),
-                                                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
-                                                    colors = TextFieldDefaults.textFieldColors(
-                                                        focusedIndicatorColor = Transparent,
-                                                        disabledIndicatorColor = Transparent,
-                                                        unfocusedIndicatorColor = Transparent,
-                                                        backgroundColor = dark400,
-                                                    ),
-                                                    modifier =  Modifier.padding(all = 16.dp).fillMaxWidth()
-                                                )
-                                            }
-
-                                        }
-                                    }
-                                }
-
-                            }
-
                         }
                     }
                 }
-
             }
         }
     }
@@ -98,10 +75,111 @@ fun BigDisplayer(acqua : Int = 0) {
     }
 }
 
+@Composable
+fun Keyboard(value: String, onValueChange: (String) -> Unit, onReset: (String) -> Unit) {
+
+    //var value by remember { mutableStateOf("") }
+
+    Column (modifier = Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally) {
+        Row (modifier = Modifier.weight(1f).fillMaxWidth(), verticalAlignment = Alignment.CenterVertically) {
+            Column (modifier = Modifier
+                .weight(2f)
+                .fillMaxWidth()
+                .padding(end = 16.dp), horizontalAlignment = Alignment.End) {
+                Row (verticalAlignment = Alignment.CenterVertically){
+                    if (value.isEmpty()) {
+                        Text("0", fontSize = 12.em, textAlign = TextAlign.End, overflow = TextOverflow.Ellipsis, maxLines = 1)
+                    }
+                    else {
+                        Text(value, fontSize = 12.em, textAlign = TextAlign.End, overflow = TextOverflow.Ellipsis, maxLines = 1)
+                    }
+                    Text("g", modifier = Modifier.padding(start = 16.dp))
+                }
+            }
+            Column(modifier = Modifier
+                .weight(1f).fillMaxHeight()
+                .clickable { onReset("") }, horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center) {
+                KeyboardKey(value = "Del")
+            }
+        }
+        Row (modifier = Modifier.weight(1f).fillMaxWidth()) {
+            Column (modifier = Modifier
+                .weight(1f).fillMaxHeight()
+                .clickable { onValueChange("7") }, horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center) {
+                KeyboardKey(value = "7")
+            }
+            Column (modifier = Modifier
+                .weight(1f).fillMaxHeight()
+                .clickable { onValueChange("8") }, horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center) {
+                KeyboardKey(value = "8")
+            }
+            Column (modifier = Modifier
+                .weight(1f).fillMaxHeight()
+                .clickable { onValueChange("9") }, horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center) {
+                KeyboardKey(value = "9")
+            }
+        }
+        Row (modifier = Modifier.weight(1f)) {
+            Column (modifier = Modifier
+                .weight(1f).fillMaxHeight()
+                .clickable { onValueChange("4") }, horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center) {
+                KeyboardKey(value = "4")
+            }
+            Column (modifier = Modifier
+                .weight(1f).fillMaxHeight()
+                .clickable { onValueChange("5") }, horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center) {
+                KeyboardKey(value = "5")
+            }
+            Column (modifier = Modifier
+                .weight(1f).fillMaxHeight()
+                .clickable { onValueChange("6") }, horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center) {
+                KeyboardKey(value = "6")
+            }
+        }
+        Row (modifier = Modifier.weight(1f)) {
+            Column (modifier = Modifier
+                .weight(1f).fillMaxHeight()
+                .clickable { onValueChange("1") }, horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center) {
+                KeyboardKey(value = "1")
+            }
+            Column (modifier = Modifier
+                .weight(1f).fillMaxHeight()
+                .clickable { onValueChange("2") }, horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center) {
+                KeyboardKey(value = "2")
+            }
+            Column (modifier = Modifier
+                .weight(1f).fillMaxHeight()
+                .clickable { onValueChange("3") }, horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center) {
+                KeyboardKey(value = "3")
+            }
+        }
+        Row (modifier = Modifier.weight(1f)) {
+            Column (modifier = Modifier
+                .weight(1f).fillMaxHeight(),
+                horizontalAlignment = Alignment.CenterHorizontally) {
+            }
+            Column (modifier = Modifier
+                .weight(1f).fillMaxHeight()
+                .clickable { onValueChange("0") }, horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center) {
+                KeyboardKey(value = "0")
+            }
+            Column (modifier = Modifier
+                .weight(1f).fillMaxHeight(),
+                horizontalAlignment = Alignment.CenterHorizontally) {
+            }
+        }
+    }
+}
+
+@Composable
+fun KeyboardKey(value: String) {
+    Text(value, color = White, fontSize = 10.em)
+}
+
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     OnePopoteTheme {
-        BigDisplayer(10)
+        KeyboardKey("1")
     }
 }
